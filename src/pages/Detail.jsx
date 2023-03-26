0eec574baa8dd7bb8c04467b801da0e31ca83576
choidami5126 <dahyun5003@kakao.com>
2023-03-26 23:44:23 +0900

    [build] 디테일 페이지 UI 및 기능 구현 중

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import Header from '../components/Header'
+import { useDispatch, useSelector } from 'react-redux'
+import styled from 'styled-components'
+import ProductSlider from '../components/ProductSlider'
 
 function Detail() {
-  return <div>Detail</div>
+  const params = useParams()
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const { products, isLoading, error } = useSelector((state) => state.products)
+  const param = params.id
+
+  return (
+    <div>
+      <Header />
+      <Top>
+        <ProductImage>
+          <ProductSlider />
+        </ProductImage>
+        <ProductInfo>호텔정보</ProductInfo>
+      </Top>
+    </div>
+  )
 }
 
 export default Detail
 
+const Top = styled.div`
+  width: 975px;
+  height: 450px;
+
+  border: 1px solid black;
+  box-sizing: border-box;
+
+  margin: auto;
+  margin-top: 110px;
+
+  display: flex;
+  flex-direction: row;
+`
+
+const ProductImage = styled.div`
+  width: 500px;
+  height: 450px;
+
+  border: 1px solid black;
+  margin-right: 50px;
+  box-sizing: border-box;
+`
+
+const ProductInfo = styled.div`
+  width: 420px;
+  height: 335px;
+
+  border: 1px solid black;
+  box-sizing: border-box;
+`
