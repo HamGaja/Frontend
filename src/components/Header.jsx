b02fe70a55bdb30b871504dd0b489b83a034ccc6
choidami5126 <dahyun5003@kakao.com>
2023-03-26 23:41:07 +0900

    [build] 공용 header, footer 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,26 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react'
+import styled from 'styled-components'
 
 function Header() {
   return (
     <StHeader>
-      <h2>여기에 페이지 제목이 들어갑니다.</h2>
+      <h2>공용 헤더 입니다.</h2>
     </StHeader>
-  );
+  )
 }
 
 const StHeader = styled.div`
-  height: 100px;
-  width: 95%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid black;
-  margin: 10px;
-`;
+  height: 72px;
+  width: 100%;
 
-export default Header;
+  position: fixed;
+  top: 0px;
+  bottom: 865px;
+
+  display: block;
+  background-color: #f7323f;
+  border: 1px solid black;
+  box-sizing: border-box;
+`
+
+export default Header
